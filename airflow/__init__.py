b594fed1da617d8bd1c365b900546e6713f66326
Kaigang Tao <tao2720@outlook.com>
2022-05-25 20:10:46 +0800

    V1.0

---
+++ airflow/__init__.py
@@ -0,0 +1,2 @@
+# /Users/kgt/code/github/PySpark-PyEcharts/airflow/constraints-3.8.txt
+# pip install apache-airflow==2.2.5 --constraint /Users/kgt/code/github/PySpark-PyEcharts/venv/constraints3.8.txt
